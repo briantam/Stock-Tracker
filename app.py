f97061012c9de2f7a8527046ae6fb85a73123f3c
Brian Tam <briantam@usc.edu>
2015-04-16 19:58:09 -0700

    Configured the portfolio widget enough to my liking

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class Application(Frame):
 
 		#Second row of widgets
 		self.newLabel	= Label(self, text="New Stock:")
-		self.entry		= Entry(self, width=22, fg="gray")
+		self.entry		= Entry(self, width=22, fg="#757575")
 		self.addButton	= Button(self, text="Add", command=self.addStock)
 
 		#Grid the second row of widgets
@@ -32,12 +32,27 @@ class Application(Frame):
 		self.entry.grid(row=1, column=1, columnspan=3)
 		self.addButton.grid(row=1, column=4)
 
-		#Add the frame that will display the portfolio
-		self.pFrame		= LabelFrame(self, text="Portfolio")
+		#Add the frame that will display the portfolio widget
+		self.pFrame	= LabelFrame(self, text="Your Portfolio", font="Verdana 18", labelanchor=N)
 		self.pFrame.grid(columnspan=10, sticky=E+W)
 		self.pFrame.columnconfigure(0, weight=1)
-		self.portfolio 	= Listbox(self.pFrame)
-		self.portfolio.grid(sticky=E+W)
+		
+		self.portfolio 	= Listbox(self.pFrame, height=15, font="Verdana 14")
+		self.portfolio["bg"] = "#b8dfd8"
+		self.portfolio["cursor"] = "hand2"
+		self.portfolio["bd"] = 2
+		self.portfolio["selectborderwidth"] = 5
+		self.portfolio["activestyle"] = "none"
+		self.portfolio.grid(sticky=E+W, columnspan=10)
+
+
+		self.money = "#1ca64a"
+		for i in range(10):
+			self.portfolio.insert(END, "Item " + str(i))
+			if i%2 == 0:
+				self.portfolio.itemconfig(i, fg=self.money, selectbackground="green")
+			else:
+				self.portfolio.itemconfig(i, fg="red", selectbackground="red")
 
 
 	def addStock(self):
@@ -48,11 +63,15 @@ class Application(Frame):
 			messagebox.showwarning("Invalid Entry", "Enter a ticker symbol with no spaces")
 			self.entry.delete(0, END)
 		else:
-			print("Adding " + self.entry.get())
+			print("Adding " + self.entry.get().strip())
 
 
 	def deleteStock(self):
 		print("Deleting stock")
+		print(self.portfolio.get(ACTIVE))
+		
+		self.portfolio.delete(ACTIVE)
+		self.portfolio.selection_set(ACTIVE)
 
 
 	#Event handler when user first clicks on the entry field
@@ -63,6 +82,14 @@ class Application(Frame):
 
 	def refresh(self):
 		print("Refresh!")
+
+		self.portfolio.delete(0, END)
+		for i in range(20):
+			self.portfolio.insert(END, "Item " + str(i))
+			if i%2 == 0:
+				self.portfolio.itemconfig(i, fg=self.money, selectbackground="green")
+			else:
+				self.portfolio.itemconfig(i, fg="red", selectbackground="red")
 
 
 	def save(self):
@@ -77,7 +104,7 @@ class Application(Frame):
 def main():
 	root = Tk()
 	root.title("Stock Widget")
-	root.geometry("350x500")
+	root.geometry("340x510")
 
 	app = Application(root)
 	root.mainloop()
